86d7058c7bace2195cb01361ad822389f91ed2fa
felipemaxplay <felipemaxplay@gmail.com>
2023-01-25 16:20:39 -0300

    create a new page

---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent {
+  @Input() photoCover: string = 'https://images.unsplash.com/photo-1536782376847-5c9d14d97cc0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8ZnJlZXxlbnwwfHwwfHw%3D&w=1000&q=80';
+  @Input() title: string = 'My news';
+  @Input() description: string = "lorem ipsum";
+}
